45fd2b28bcf519237867299522354b0fa8823188
minzzun99 <skg06123@naver.com>
2025-11-01 23:48:29 +0900

    feat: Application 클래스 구현
    - main 메서드에서 LottoController 실행
    - LottoService, LottoNumberGenerator 객체 생성 및 의존성 주입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.strategy.LottoNumberGenerator;
+import lotto.domain.strategy.NumberGenerator;
+import lotto.service.LottoService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberGenerator numberGenerator = new LottoNumberGenerator();
+        LottoService lottoService = new LottoService(numberGenerator);
+        LottoController lottoController = new LottoController(lottoService);
+        lottoController.run();
     }
 }
