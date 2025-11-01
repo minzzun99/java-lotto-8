61576e1266accc2a501e68bdbcdbc2a79256791d
minzzun99 <skg06123@naver.com>
2025-11-02 00:46:25 +0900

    refactor: PrintMessage 출력 형식에 맞게 줄바꿈 문자 추가

--- src/main/java/lotto/view/PrintMessage.java
+++ src/main/java/lotto/view/PrintMessage.java
@@ -2,13 +2,13 @@ package lotto.view;
 
 public enum PrintMessage {
     INPUT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
 
-    PURCHASE_RESULT("%d개를 구매했습니다.%n"),
+    PURCHASE_RESULT("%n%d개를 구매했습니다.%n"),
     PURCHASE_LOTTO_NUMBERS("[%s]%n"),
 
-    WINNING_RESULT_HEADER("당첨 통계"),
+    WINNING_RESULT_HEADER("\n당첨 통계"),
     WINNING_RESULT_SEPARATOR("---"),
     WINNING_RESULT_FORMAT("%s (%,d원) - %d개%n"),
     TOTAL_RETURN("총 수익률은 %.1f%%입니다.");
