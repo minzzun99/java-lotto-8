47f679886face735eab19ea020509f6b83153717
minzzun99 <skg06123@naver.com>
2025-11-01 22:05:08 +0900

    refactor: Rank enum 구조 변경
    - enum 순서를 출력 순서에 맞게 변경
    - message 필드 추가
    - getter 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,21 +1,23 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FIFTH(3, false, 5_000, "3개 일치"),
+    FOURTH(4, false, 50_000, "4개 일치"),
+    THIRD(5, false, 1_500_000, "5개 일치"),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, false, 2_000_000_000, "6개 일치"),
+    NONE(0, false, 0, "");
 
     private final int matchCount;
     private final boolean isBonusMatch;
     private final int prize;
+    private final String message;
 
-    Rank(int matchCount, boolean isBonusMatch, int prize) {
+    Rank(int matchCount, boolean isBonusMatch, int prize, String message) {
         this.matchCount = matchCount;
         this.isBonusMatch = isBonusMatch;
         this.prize = prize;
+        this.message = message;
     }
 
     public static Rank from(int matchCount, boolean isBonusMatch) {
@@ -39,5 +41,13 @@ public enum Rank {
             return SECOND;
         }
         return THIRD;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
